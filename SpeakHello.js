248e9cda9a202ad96f9e92d0a47a8b545c723187
nkiomi <nabilakc@gmail.com>
2016-07-31 16:05:05 -0300

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function () {
+    console.log(speakWord + " " + names[i]);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
+
